4c204014a1cdd667901822877b7f0f95db2673e6
dohyeon <ehgus97334@gmail.com>
2026-05-21 13:04:49 +0900

    spring boot 파일 구성

---
+++ auth_post/src/main/java/com/dohyeon/auth_post/AuthPostApplication.java
@@ -0,0 +1,13 @@
+package com.dohyeon.auth_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthPostApplication.class, args);
+	}
+
+}
---
+++ auth_post/src/test/java/com/dohyeon/auth_post/AuthPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dohyeon.auth_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
